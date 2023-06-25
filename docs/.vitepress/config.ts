c5ef7098643f00d7ff94bdcc04f2abae99932c37
qcsy <yiwenyun@outlook.com>
2023-06-25 10:28:21 +0800

    build project

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Find myself",
+  description: "try to find myself,we all need this",
+  base: "/orion/",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' },
+          { text: 'By me', link: '/sth by me' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+      { icon: 'facebook', link: 'https://github.com/vuejs/vitepress' },
+
+    ]
+  }
+})
---
+++ docs/sth by me.md
@@ -0,0 +1,5 @@
+### this is first content
+
+just try it
+
+but i can't read it can you tell me the reason
